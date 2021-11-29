e1f3b2455b620b41784efbe37d012cd27005f355
Lucelia <lucelia1976@gmail.com>
2021-11-29 16:35:38 -0300

    ExercicioVariaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,38 @@
+// Exercicio de interpretacao codigo
+// Pergunta 1 - Resposta: 10 5
+// Pergunta 2 - Resposta: 10 10 10
+// Pergunta 3 - Resposta: lep p - jornadaTrabalho / let t - remuneracaoDiaria
+
+/* const nome = ""
+const idade = ""
+
+console.log("nome", typeof nome)
+console.log("idade", typeof idade) */
+
+//2a parte, exercicio 1 - Letra D - foi impresso string pois não identificou numeração
+
+/* const nome=prompt("qual seu nome?")
+const idade=prompt("qual sua idade?") */
+
+/* console.log(nome)
+console.log(idade) */
+
+/* console.log("nome", typeof nome)
+console.log("idade", typeof idade) */
+
+/*2aa parte, exercicio 1 - Letra F - Permaneceu String para ambos, acredito que por não ter
+lançado a idade no const idade */
+
+/* console.log("Olá, meu nome é", nome, "tenho", idade, "de idade") */
+
+
+
+
+
+
+
+
+
+
+
+
