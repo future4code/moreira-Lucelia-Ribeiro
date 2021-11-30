cdbcd1ed58c4979ebeae4143eceab72008f6354d
Lucelia <lucelia1976@gmail.com>
2021-11-30 16:25:33 -0300

    Exercicio Operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,80 @@
+/*1. Leia o código abaixo. Indique todas as mensagens impressas no console, **SEM EXECUTAR o programa**.
+    a. false
+    b. false
+    c. true
+    d. boolean*/
+
+/* 2. Seu colega se aproxima de você falando que o código dele não funciona como devia.  
+    Vamos ajudá-lo: consegue perceber algum problema? O que será impresso no console?
+
+    Irá imprimir os dois numeros digitados e não a soma, pois não foi alterado ara numero */
+
+/* 3. Para o exercício anterior, sugira ao seu colega uma solução para que o valor impresso
+no console seja, de fato, a soma dos dois números.
+
+    let primeiroNumero = Number(prompt ("Digite um numero!"))
+    let segundoNumero = Number(prompt ("Digite outro numero!"))
+    */
+
+/* EXERCICIO DE ESCRITA DE CÓDIGO
+
+1. Faça um programa que:
+    a) Pergunte a idade do usuário
+    b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+    c) **Imprima na console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?",
+    seguido pela resposta (`true` ou `false`)
+    d) **Imprima na console** a diferença de idade (não tem problema se sair um número negativo)*/
+
+   /*  const idadeDoUsuario = Number(prompt("Qual sua idade?"))
+    const idadeSeuAmigo = Number(prompt("Qual idade do seu melhor amigo?"))
+
+    let idade = idadeDoUsuario > idadeSeuAmigo
+    let diferente = idadeDoUsuario - idadeSeuAmigo
+
+    console.log("Sua idade é maior do que a do seu melhor amigo?", idade )
+    console.log("Qual a diferença de idade entre vocês?", diferente) 
+    
+3. Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console 
+    a) A idade do usuário em meses
+    b) A idade do usuário em dias
+    c) A idade do usuário em horas
+
+    const idadeDoUsuario = Number(prompt("Qual sua idade?"))
+
+    const meses = idadeDoUsuario * 12
+    const dias = meses * 30
+    const horas = dias * 24
+
+    console.log
+    console.log("Idade em meses:", meses, "Idade em dias:", dias, "Idade em horas", horas)
+
+4. Faça um programa que pergunte ao usuário dois números. Em seguida, faça as operações e imprima no console
+ as seguintes mensagens seguidas pelo `true` ou `false`:
+    
+    O primeiro numero é maior que segundo? true
+    O primeiro numero é igual ao segundo? false
+    O primeiro numero é divisível pelo segundo? true
+    O segundo numero é divisível pelo primeiro? true
+    
+    obs: O true ou false vai depender dos números inseridos e do resultado das operações.*/
+
+    /* const primeiro = Number(prompt("Digite um numero:"))    
+    const segundo = Number(prompt("Digite outro numero:"))
+
+    const n1 = primeiro > segundo
+    const n2 = primeiro === segundo
+    const n3 = primeiro & segundo
+    const n4 = segundo & primeiro
+
+    console.log("O primeiro numero é maior que segundo?", n1)
+    console.log("O primeiro numero é igual ao segundo?", n2)
+    console.log("O primeiro numero é divisivel pelo segundo?", n3 === n4)
+    console.log("O primeiro numero é divisivel pelo segundo?", n4 === n3) */
+
+    
+
+
+
+
+
+
