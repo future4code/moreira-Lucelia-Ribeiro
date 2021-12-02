7b78c0575895f77fa12080791a214eec036e6a94
Lucelia <lucelia1976@gmail.com>
2021-12-02 16:50:41 -0300

    exercicio funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,41 @@
+/* Tente responder os exercícios dessa seção sem executar o código. 
+Se ficar muito difícil, pode rodar no seu computador **para analisar e pensar sobre o resultado.** 
+
+1. Leia o código abaixo*/
+   /*  
+    function minhaFuncao(variavel) {
+    	return variavel * 5
+    }
+    
+    console.log(minhaFuncao(2))
+    console.log(minhaFuncao(10)) */
+       
+    /*a) O que vai ser impresso no console?
+    10 e 50
+    
+    b) O que aconteceria se retirasse os dois `console.log` e simplesmente invocasse a 
+    função `minhaFuncao(2)` e `minhaFuncao(10)`? O que apareceria no console?
+    Não funcionará e  no console aparecerá erro*/
+
+   
+  
+ 
+/* 2. Leia o código abaixo */
+
+/* let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) */
+
+/* a. Explique o que essa função faz e qual é sua utilidade
+Ela coloca o texto em caixa baixa, caso o usuário digite diferente, ele padroniza
+procura na frase a palavra "cenoura" e retorna falso ou verdadeiro
+
+b. Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+     i.   `Eu gosto de cenoura` - true
+     ii.  `CENOURA é bom pra vista` - true
+     iii. `Cenouras crescem na terra` - true */
