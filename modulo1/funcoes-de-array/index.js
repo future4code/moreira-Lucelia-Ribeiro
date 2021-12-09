500b266b1a59b78b624f5a5711e6df52cdb93a59
Lucelia <lucelia1976@gmail.com>
2021-12-09 20:32:00 -0300

    alteração

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -6,7 +6,7 @@
 
 // 1. Leia o código abaixo
 
-const usuarios = [
+/* const usuarios = [
     { nome: "Amanda Rangel", apelido: "Mandi" },
     { nome: "Laís Petra", apelido: "Laura" },
     { nome: "Letícia Chijo", apelido: "Chijo" }
@@ -14,13 +14,14 @@ const usuarios = [
   
   const novoArrayA = usuarios.map((item, index, array) => {
      console.log(item, index, array)
-  })
+  }) */
 
-  //a) O que vai ser impresso no console?
+  //a) O que vai ser impresso no console? A posição no nome, seguido
+  //do nome e apelido
 
   //2. Leia o código abaixo
     
-    const usuarios = [
+/*    const usuarios = [
     { nome: "Amanda Rangel", apelido: "Mandi" },
     { nome: "Laís Petra", apelido: "Laura" },
     { nome: "Letícia Chijo", apelido: "Chijo" },
@@ -30,26 +31,26 @@ const usuarios = [
      return item.nome
   })
   
-  console.log(novoArrayB)
+  console.log(novoArrayB) */
   
-  // a) O que vai ser impresso no console?
+  // a) O que vai ser impresso no console? Retornara somente o nome de cada pessoa
 
   // 3. Leia o código abaixo
 
- const usuarios = [
+ /* const usuarios = [
   { nome: "Amanda Rangel", apelido: "Mandi" },
   { nome: "Laís Petra", apelido: "Laura" },
   { nome: "Letícia Chijo", apelido: "Chijo" },
 ]
 
 const novoArrayC = usuarios.filter((item, index, array) => {
-   return item.apelido !== "Chijo"
+   return item.apelido !== "Chijo" 
 })
 
-console.log(novoArrayC)
+console.log(novoArrayC) */
 
-// a) O que vai ser impresso no console?
-
+// a) O que vai ser impresso no console? irá imprimir os dados que não
+//tem Chijo
 ///////////////////////////////////////////////////////////////////////
 
 //Exercícios de escrita de código
@@ -68,12 +69,34 @@ const pets = [
 ]
 
 //a) Crie um novo array que contenha apenas o nome dos doguinhos
+
+/* let nomeDog = pets.map ((pet) => {
+   return pet.nome
+   
+})
+console.log(nomeDog) */
+
+
 //b) Crie um novo array apenas com os [cachorros salsicha]
 //(https://www.youtube.com/watch?v=YQ404vwjzus)
+
+/* let salsicha = pets.filter((tipo) => {
+      return tipo.raca === "Salsicha"
+})
+console.log(salsicha) */
+
 //c) Crie um novo array que possuirá mensagens para enviar para todos os 
 //clientes que são poodles. 
 //A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para 
 //tosar o/a `[NOME]`!"
+
+/* let racaPoodle = pets.filter((tipo) => {
+   return tipo.raca === "Poodle"
+})
+let cupomDesconto = racaPoodle.map((tipo) => {
+   return `Você ganhou um cupom de 10% de desconto para tosar o/a ${tipo.nome} !`
+})
+console.log(cupomDesconto) */
 
 /* Resultados esperados em cada item
     // item A
@@ -113,15 +136,36 @@ const pets = [
         { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
         { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
      ]
-
+ 
 //a) Crie um novo array que contenha apenas o nome de cada item
+
+/* let nomeProdutos = produtos.map ((prod) => {
+   return prod.nome
+}
+)
+console.log(nomeProdutos) */
 
 //b) Crie um novo array que contenha um objeto com o nome e o preço de cada item,
 //aplicando 5% de desconto em todos eles
 
+/* let desconto = produtos.map((prod) => {
+   return prod.nome + " " + prod.preco * 0.95
+})
+console.log(desconto) */
+
 //c) Crie um novo array que contenha apenas os objetos da categoria Bebidas
 
+/* let bebidas = produtos.filter((bebi) => {
+   return bebi.categoria === "Bebidas"
+})
+console.log(bebidas) */
+
 //d) Crie um novo array que contenha apenas os objetos cujo nome contenha a palavra "Ypê"
+
+let detergente = produtos.filter((sabao) =>{
+   return sabao.nome.includes("Ypê")
+})
+console.log(detergente)
 
 //e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". Esse array
 // deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
